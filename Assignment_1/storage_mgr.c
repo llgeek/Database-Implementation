eccac878b148ca7ca3d09e7ab476ed6c60b3395b
llgeek <angleflycll@gmail.com>
2017-09-14 17:35:19 -0500

    block writing

--- Assignment_1/storage_mgr.c
+++ Assignment_1/storage_mgr.c
@@ -80,3 +80,74 @@ int main () {
 	  return RC;
   } 
 }
+
+
+
+/***
+***** author: Linlin Chen
+***** email: lchen96@hawk.iit.edu
+***/
+
+/**
+ * Write pages to disk using absolute position
+ * @param  pageNum number of pages to be written
+ * @param  fHandle file handler to be written to
+ * @param  memPage page handle to be written from
+ * @return Error Code         
+ */
+RC writeBlock (int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage) {
+	int fileseeker = 0;
+
+	/**
+	 *  checks the validity of the pageNum 
+	 *  Whether it's negative or exceed the maximal number of pages for this file 
+	**/
+	if (pageNum <= 0 || pageNum > (fHandle->totalNumPages) ) {
+		return RC_WRITE_FAILED;
+	}
+
+	/** seek  file pointer*/
+	fileseeker = fseek(fHandle->mgmtInfo, (pageNum + 1))
+
+}
+
+
+
+/**
+ * Write pages using current position
+ * @param  fHandle [description]
+ * @param  memPage [description]
+ * @return         [description]
+ */
+RC writeCurrentBlock (SM_FileHandle *fHandle, SM_PageHandle memPage) {
+
+}
+
+
+
+/**
+ * Increase the number of pages in the file by one
+ * @param  fHandle [description]
+ * @return         [description]
+ */
+RC appendEmptyBlock (SM_FileHandle *fHandle) {
+
+}
+
+
+
+
+
+
+/**
+ * Increase the size to numberOfPages in the file size has less than numbnerOfPages pages
+ * @param  numberOfPages [description]
+ * @param  fHandle       [description]
+ * @return               [description]
+ */
+RC ensureCapacity (int numberOfPages, SM_FileHandle *fHandle) {
+
+}
+
+
+
