9939a6067a40aa28743be2f37ee114b67b499f7a
llgeek <angleflycll@gmail.com>
2017-10-25 01:21:38 -0500

    fix one merge error

--- Assignment_2/buffer_mgr.c
+++ Assignment_2/buffer_mgr.c
@@ -2,67 +2,6 @@
 #include <strlib.h>
 
 #include "buffer_mgr.h"
-<<<<<<< HEAD
-
-
-
-
-
-//Start Buffer Manager Interface Access Pages
-/**
- * Linlin Chen
- * A20348195
- * lchen96@hawk.iit.edu
- */
-
-/**
- * pins the page with page number pageNum
- * @param  bm: the buffer manager pool
- * @param  page: the page stored the page file content
- * @return      any error code
- */
-RC pinPage (BM_BufferPool *const bm, BM_PageHandle *const page, 
-	    const PageNumber pageNum) {
-
-
-}
-
-
-/**
- * unpins the page page
- * @param  bm: the buffer manager pool
- * @param  page: the page handle, the pagenum will be used to figure out which page will be unpined
- * @return      error code
- */
-RC unpinPage (BM_BufferPool *const bm, BM_PageHandle *const page) {
-
-}
-
-
-/**
- * marks a page as dirty.
- * @param  bm: buffer manager
- * @param  page: page handle
- * @return    error code
- */
-RC markDirty (BM_BufferPool *const bm, BM_PageHandle *const page) {
-
-}
-
-
-/**
- * write the current content of the page back to the page file on disk
- * @param  bm: the buffer manager
- * @param  page: the page handle
- * @return      error code
- */
-RC forcePage (BM_BufferPool *const bm, BM_PageHandle *const page) {
-
-}
-//End Buffer Manager Interface Access Pages
-
-
-=======
 #include "storage_mgr.h"
 
 
@@ -206,4 +145,59 @@ RC forceFlushPool(BM_BufferPool *const bm){
     return RC_OK;
     
 }
->>>>>>> 42966980a205be6ca9e9b99ed47f75527932c179
+
+
+
+
+//Start Buffer Manager Interface Access Pages
+/**
+ * Linlin Chen
+ * A20348195
+ * lchen96@hawk.iit.edu
+ */
+
+/**
+ * pins the page with page number pageNum
+ * @param  bm: the buffer manager pool
+ * @param  page: the page stored the page file content
+ * @return      any error code
+ */
+RC pinPage (BM_BufferPool *const bm, BM_PageHandle *const page, 
+	    const PageNumber pageNum) {
+
+
+}
+
+
+/**
+ * unpins the page page
+ * @param  bm: the buffer manager pool
+ * @param  page: the page handle, the pagenum will be used to figure out which page will be unpined
+ * @return      error code
+ */
+RC unpinPage (BM_BufferPool *const bm, BM_PageHandle *const page) {
+
+}
+
+
+/**
+ * marks a page as dirty.
+ * @param  bm: buffer manager
+ * @param  page: page handle
+ * @return    error code
+ */
+RC markDirty (BM_BufferPool *const bm, BM_PageHandle *const page) {
+
+}
+
+
+/**
+ * write the current content of the page back to the page file on disk
+ * @param  bm: the buffer manager
+ * @param  page: the page handle
+ * @return      error code
+ */
+RC forcePage (BM_BufferPool *const bm, BM_PageHandle *const page) {
+
+}
+//End Buffer Manager Interface Access Pages
